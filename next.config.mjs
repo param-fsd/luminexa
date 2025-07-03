310beec17c6c3fba121c2837c69c1d348fd038ed
param <psprashantanilgod@gmail.com>
2025-07-03 10:58:05 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.dribbble.com"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Temporarily ignore ESLint errors during builds
+    domains: ["cdn.dribbble.com"], // Add the external image domain here
   },
 };
 
